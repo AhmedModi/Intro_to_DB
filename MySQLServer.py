7eb297bd145e5888ae748ca7ecc782538c0d8e84
Ahmed Modee <46077468+AhmedModi@users.noreply.github.com>
2025-08-14 13:45:28 +0100

    Update MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -5,7 +5,6 @@ If the database already exists, it will not fail.
 """
 
 import mysql.connector
-from mysql.connector import Error
 
 def create_database():
     try:
@@ -21,14 +20,17 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error while connecting to MySQL: {e}")
 
     finally:
         # Close connection
-        if connection.is_connected():
-            cursor.close()
-            connection.close()
+        try:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
+        except NameError:
+            pass
 
 if __name__ == "__main__":
     create_database()
